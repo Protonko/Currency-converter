d1e1c5eb89cff0802198d5be74fe10ecbaecb9fb
Egor Ermolaev <probegrofanston@mail.ru>
2020-09-12 22:34:11 +0300

    optimization of lists render

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { CONTACTS } from './contactsData';
 
 class About extends React.Component {
-  render() {
-    const renderContacts = elem =>  <li key={elem}>{elem}: {CONTACTS[elem]}</li>;
+  constructor(props) {
+    super(props);
 
+    this.renderContacts = this.renderContacts.bind(this);
+  }
+
+  renderContacts(elem, index) {
+    return (
+      <li key={index}>{elem}: {CONTACTS[elem]}</li>
+    )
+  }
+
+  render() {
     return (
       <div>
         <ul>
-          {Object.keys(CONTACTS).map(renderContacts)}
+          {Object.keys(CONTACTS).map(this.renderContacts)}
         </ul>
       </div>
     );
